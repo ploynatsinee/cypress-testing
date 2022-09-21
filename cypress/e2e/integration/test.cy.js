7c2249ebdeba83959481d4798a6686f32dd70f8a
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-21 16:51:49 +0700

    add input

--- cypress/e2e/integration/test.cy.js
+++ cypress/e2e/integration/test.cy.js
@@ -1,5 +1,11 @@
 describe('empty spec', () => {
-  it('visit', () => {
+  it('visit website', () => {
     cy.visit('https://fitfriends.vercel.app')
+  })
+  it('username input', () => {
+    cy.get(':nth-child(1) > input').type('ploynatsinee')
+  })
+  it('password input', () => {
+    cy.get(':nth-child(2) > input').type('1234')
   })
 })
